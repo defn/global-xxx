9af7ef3bab657c415c20ceea50f7b3e187b2767e
Tom Bombadil <amanibhavam@destructuring.org>
2016-07-02 22:58:27 -0700

    pass args to global module

--- fogg.tf
+++ fogg.tf
@@ -5,6 +5,16 @@ module "global" {
 
   bucket_remote_state = "${var.bucket_remote_state}"
   az_names = "${var.az_names}"
+  aws_account_id = "${var.aws_account_id}"
+  aws_region = "${var.aws_region}"
+}
+
+output "aws_account_id" {
+  value = "${module.global.aws_account_id}"
+}
+
+output "aws_region" {
+  value = "${module.global.aws_region}"
 }
 
 output "bucket_remote_state" {
